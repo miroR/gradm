c8322f8d2211da851b329bb2b92657213bf6a418
spender <spender>
2003-08-07 18:35:25 +0000

    update

--- gradm_newlearn.c
+++ gradm_newlearn.c
@@ -1042,7 +1042,7 @@ show_ips:
 					fprintf(stream, "\t\"%s\"\t\t\t\t%s\n", node->filename, modes);
 				else if (i < 13)
 					fprintf(stream, "\t\"%s\"\t\t\t%s\n", node->filename, modes);
-				else if (i < 23)
+				else if (i < 27)
 					fprintf(stream, "\t\"%s\"\t\t%s\n", node->filename, modes);
 				else
 					fprintf(stream, "\t\"%s\"\t%s\n", node->filename, modes);
@@ -1051,7 +1051,7 @@ show_ips:
 				fprintf(stream, "\t%s\t\t\t\t%s\n", node->filename, modes);
 			else if (i < 13)
 				fprintf(stream, "\t%s\t\t\t%s\n", node->filename, modes);
-			else if (i < 23)
+			else if (i < 27)
 				fprintf(stream, "\t%s\t\t%s\n", node->filename, modes);
 			else
 				fprintf(stream, "\t%s\t%s\n", node->filename, modes);
