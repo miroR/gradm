c2653360a31a310984e3554adda21c2fda0429ff
spender <spender>
2005-05-05 01:20:51 +0000

    update gradm_pam rules

--- gradm_adm.c
+++ gradm_adm.c
@@ -147,7 +147,10 @@ add_gradm_pam_acl(struct role_acl *role)
 	add_proc_object_acl(current_subject, "/usr/share/zoneinfo", proc_object_mode_conv("r"), GR_FEXIST);
 	add_proc_object_acl(current_subject, "/etc/nsswitch.conf", proc_object_mode_conv("r"), GR_FEXIST);
 	add_proc_object_acl(current_subject, "/dev/urandom", proc_object_mode_conv("r"), GR_FEXIST);
+	add_proc_object_acl(current_subject, "/proc", proc_object_mode_conv(""), GR_FEXIST);
+	add_proc_object_acl(current_subject, "/dev", proc_object_mode_conv(""), GR_FEXIST);
 	add_proc_object_acl(current_subject, "/dev/log", proc_object_mode_conv("rw"), GR_FEXIST);
+	add_proc_object_acl(current_subject, "/dev/null", proc_object_mode_conv("rw"), GR_FEXIST);
 	add_proc_object_acl(current_subject, "/lib", proc_object_mode_conv("rx"), GR_FEXIST);
 	add_proc_object_acl(current_subject, "/usr/lib", proc_object_mode_conv("rx"), GR_FEXIST);
 	add_proc_object_acl(current_subject, "/lib64", proc_object_mode_conv("rx"), GR_FEXIST);
