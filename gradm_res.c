46c56d37219e4f4c1de45cb7b9c1cf42befc99b2
spender <spender>
2009-01-25 19:08:44 +0000

    fix unlimited RES_NOFILE limit

--- gradm_res.c
+++ gradm_res.c
@@ -139,8 +139,7 @@ add_res_acl(struct proc_acl *subject, const char *name,
 	lim.rlim_max = conv_res(hard);
 
 	if (!strcmp(name, "RES_NOFILE") &&
-	    (((lim.rlim_cur != ~0UL) && (lim.rlim_cur > NR_OPEN)) ||
-	     ((lim.rlim_max != ~0UL) && (lim.rlim_max > NR_OPEN)))) {
+	    ((lim.rlim_cur > NR_OPEN) || (lim.rlim_max > NR_OPEN))) {
 		fprintf(stderr, "Limits for RES_NOFILE cannot be larger "
 			"than %u.\n", NR_OPEN);
 		exit(EXIT_FAILURE);
