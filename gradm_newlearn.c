f4ce004f28e3cce5a4c71fc691dc01e4fae66704
spender <spender>
2009-04-14 17:56:26 +0000

    fix learning crash on NULL node subject

--- gradm_newlearn.c
+++ gradm_newlearn.c
@@ -1204,7 +1204,7 @@ show_ips:
 			display_ip_tree(connect, GR_IP_CONNECT, stream);
 		else
 			fprintf(stream, "\tconnect\tdisabled\n");
-		if (node->subject->inaddr_any_override) {
+		if (node->subject != NULL && node->subject->inaddr_any_override) {
 			struct in_addr addr;
 			addr.s_addr = node->subject->inaddr_any_override;
 			fprintf(stream, "\tip_override\t%s\n", inet_ntoa(addr));
