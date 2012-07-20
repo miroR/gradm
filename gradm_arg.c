4f7c7f386f1a30d1e2d1f59604996ea889871317
Brad Spengler <spender@grsecurity.net>
2012-07-20 15:40:15 -0400

    Allocate buffer for learn log buffering

--- gradm_arg.c
+++ gradm_arg.c
@@ -163,10 +163,11 @@ static FILE *open_learn_log(char *learn_log)
 		}
 	}
 
-
-	// buffer the learn log if possible
-	setvbuf(learnfile, learn_log_buffer, _IOFBF, LEARN_LOG_BUFFER_SIZE);
-
+	learn_log_buffer = malloc(LEARN_LOG_BUFFER_SIZE);
+	if (learn_log_buffer) {
+		// buffer the learn log if possible
+		setvbuf(learnfile, learn_log_buffer, _IOFBF, LEARN_LOG_BUFFER_SIZE);
+	}
 
 	return learnfile;
 }
