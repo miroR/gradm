miroR/gradm

commit 4b923540573b90c0b2274d510e4948aa9c962775

ensure gradm password is null terminated when created, found by coverity.  Impact is only a potential crash when creating a password 128 characters in length -- not security relevant.

gradm_pw.c
@@ -105,7 +105,7 @@ get_user_passwd(struct gr_pw_entry *entry, int mode)
 			tcsetattr(STDIN_FILENO, TCSANOW, &term);
 		}
 
-		if ((read(STDIN_FILENO, entry->passwd, GR_PW_LEN)) < 0) {
+		if ((read(STDIN_FILENO, entry->passwd, GR_PW_LEN - 1)) < 0) {
 			fprintf(stderr,
 				"\nError reading password from user.\n");
 			term.c_lflag |= ECHO;
@@ -119,7 +119,7 @@ get_user_passwd(struct gr_pw_entry *entry, int mode)
 		term.c_lflag |= ECHO;
 		tcsetattr(STDIN_FILENO, TCSANOW, &term);
 
-		entry->passwd[strlen((char *)entry->passwd) - 1] = '\0';
+		entry->passwd[GR_PW_LEN - 1] = '\0';
 
 		if ((strlen((char *)entry->passwd) < 6) && mode == 1) {
 			fprintf(stderr,

commit 63017cb29510c1ded5308ca0a32b8d33f4c05d7c

fix broken password authentication caused by recent commit 4b923540573b90c0b2274d510e4948aa9c962775
It was not possible to authenticate to the RBAC system as I misread
the code and didn't notice it was stripping the trailing newline

gradm_pw.c
@@ -120,6 +120,8 @@ get_user_passwd(struct gr_pw_entry *entry, int mode)
 		tcsetattr(STDIN_FILENO, TCSANOW, &term);
 
 		entry->passwd[GR_PW_LEN - 1] = '\0';
+		/* strip newline */
+		entry->passwd[strlen((char *)entry->passwd) - 1] = '\0';
 
 		if ((strlen((char *)entry->passwd) < 6) && mode == 1) {
 			fprintf(stderr,